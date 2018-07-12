2c4faf4a1e6e3fb04f90ccf6ed1ba598c4e358dd
Я <rmacedoa@gmail.com>
2018-07-12 15:35:32 -0500

    configuracion de las bases de datos, funcion get,post

--- server.py
+++ server.py
@@ -4,35 +4,86 @@ from sqlalchemy import create_engine
 from json import dumps
 from flask.ext.jsonpify import jsonify
 
-db_connect = create_engine('sqlite:///chinook.db')
+
+db_connect = create_engine('sqlite:///condor.db')
 app = Flask(__name__)
 api = Api(app)
 
-class Employees(Resource):
+class Usuario(Resource):
     def get(self):
         conn = db_connect.connect() # connect to database
-        query = conn.execute("select * from employees") # This line performs query and returns json result
-        return {'employees': [i[0] for i in query.cursor.fetchall()]} # Fetches first column that is Employee ID
+        query = conn.execute("select id, nombre, skin, id_rep_skins from usuario;")
+        result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+        return jsonify(result)
+    #funcion post
 
-class Tracks(Resource):
+    def post(self):
+        conn = db_connect.connect()
+        print(request.json)
+        Nombre = request.json['Nombre']
+        Skin = request.json['Skin']
+        query = conn.execute("insert into usuario values(null,'{0}')".format(Nombre,Skin))
+        return {'status':'success'}
+
+class Partida(Resource):
     def get(self):
-        conn = db_connect.connect()
-        query = conn.execute("select trackid, name, composer, unitprice from tracks;")
+        conn = db_connect.connect() # connect to database
+        query = conn.execute("select id, id_nivel, progreso_realizado, monedas_recolectadas, letras_recolectadas from partida;")
         result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
         return jsonify(result)
 
-class Employees_Name(Resource):
-    def get(self, employee_id):
-        conn = db_connect.connect()
-        query = conn.execute("select * from employees where EmployeeId =%d "  %int(employee_id))
+class Seleccionar(Resource):
+    def get(self):
+        conn = db_connect.connect() # connect to database
+        query = conn.execute("select id, id_usuario, id_mapa, score from seleccionar;")
         result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
         return jsonify(result)
 
+class Mapa(Resource):
+    def get(self):
+        conn = db_connect.connect() # connect to database
+        query = conn.execute("select id, id_nivel, progreso_realizado, monedas_recolectadas, letras_recolectadas from mapa ;")
+        result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+        return jsonify(result)
 
-api.add_resource(Employees, '/employees') # Route_1
-api.add_resource(Tracks, '/tracks') # Route_2
-api.add_resource(Employees_Name, '/employees/<employee_id>') # Route_3
+class Nivel(Resource):
+    def get(self):
+        conn = db_connect.connect() # connect to database
+        query = conn.execute("select id, nombre, numero_monedas, numero_letras from nivel ;")
+        result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+        return jsonify(result)
 
+class Selector_Partida(Resource):
+    def get(self):
+        conn = db_connect.connect() # connect to database
+        query = conn.execute("select id, id_usuario, id_partida from Selector_Partida;")
+        result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+        return jsonify(result)
+
+class Tienda(Resource):
+    def get(self):
+        conn = db_connect.connect() # connect to database
+        query = conn.execute("select id, id_usuario,id_rep_skins,monedas_disponibles from tienda;")
+        result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+        return jsonify(result)
+
+class Repositorio_Skins(Resource):
+    def get(self):
+        conn = db_connect.connect() # connect to database
+        query = conn.execute("select id, id_usuario, id_tienda, skin from Repositorio_Skins;")
+        result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+        return jsonify(result)
+#por completar
+
+
+api.add_resource(Usuario, '/usuario') # Route_1
+api.add_resource(Partida, '/partida') # Route_2
+api.add_resource(Seleccionar, '/seleccionar') # Route_3
+api.add_resource(Mapa, '/mapa') # Route_4
+api.add_resource(Nivel, '/nivel') # Route_5
+api.add_resource(Selector_Partida, '/selector_partida') # Route_6
+api.add_resource(Tienda, '/tienda') # Route_7
+api.add_resource(Repositorio_Skins, '/repositorio') # Route_8
 
 if __name__ == '__main__':
      app.run(port=5002)
